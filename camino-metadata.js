a84d5850a55d8666f5bef5e320c86e7cadd05efe
Sophie Sadler <sophiefsadler@gmail.com>
2025-07-29 21:57:45 +0100

    Broke data file up to avoid size issue

---
+++ camino-metadata.js
@@ -0,0 +1,263 @@
+const caminoMetadata = [
+  {
+    "day": 0,
+    "start": "Arrival in",
+    "end": "Saint-Jean-Pied-de-Port",
+    "distance": 0,
+    "diaryFile": "diaries/day_00.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/blob/7c2f60c9d318ae882ffae5207e228da2f0b05959/images/story_panel/Day_00.jpg",
+    "center_coord": [
+      43.1636,
+      -1.2372
+    ],
+    "zoom_level": 15
+  },
+  {
+    "day": 1,
+    "start": "SJPDP",
+    "end": "Roncesvalles",
+    "distance": 26.0,
+    "diaryFile": "diaries/day_01.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_01.jpg"
+  },
+  {
+    "day": 2,
+    "start": "Roncesvalles",
+    "end": "Zubiri",
+    "distance": 23.7,
+    "diaryFile": "diaries/day_02.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_02.jpg"
+  },
+  {
+    "day": 3,
+    "start": "Zubiri",
+    "end": "Pamplona",
+    "distance": 22.7,
+    "diaryFile": "diaries/day_03.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_03.jpg"
+  },
+  {
+    "day": 4,
+    "start": "Pamplona",
+    "end": "Puente La Reina",
+    "distance": 26.8,
+    "diaryFile": "diaries/day_04.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_04.jpg"
+  },
+  {
+    "day": 5,
+    "start": "Puente La Reina",
+    "end": "Estella",
+    "distance": 24.0,
+    "diaryFile": "diaries/day_05.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_05.jpg"
+  },
+  {
+    "day": 6,
+    "start": "Estella",
+    "end": "Los Arcos",
+    "distance": 23.0,
+    "diaryFile": "diaries/day_06.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_06.jpg"
+  },
+  {
+    "day": 7,
+    "start": "Los Arcos",
+    "end": "Logro\u00f1o",
+    "distance": 29.0,
+    "diaryFile": "diaries/day_07.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_07.jpg"
+  },
+  {
+    "day": 8,
+    "start": "Logro\u00f1o",
+    "end": "N\u00e1jera",
+    "distance": 32.5,
+    "diaryFile": "diaries/day_08.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_08.jpg"
+  },
+  {
+    "day": 9,
+    "start": "N\u00e1jera",
+    "end": "Santo Domingo de la Calzada",
+    "distance": 22.3,
+    "diaryFile": "diaries/day_09.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_09.jpg"
+  },
+  {
+    "day": 10,
+    "start": "Santo Domingo de la Calzada",
+    "end": "Belorado",
+    "distance": 24.4,
+    "diaryFile": "diaries/day_10.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_10.jpg"
+  },
+  {
+    "day": 11,
+    "start": "Belorado",
+    "end": "San Juan de Ortega",
+    "distance": 25.0,
+    "diaryFile": "diaries/day_11.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_11.jpg"
+  },
+  {
+    "day": 12,
+    "start": "San Juan de Ortega",
+    "end": "Burgos",
+    "distance": 23.2,
+    "diaryFile": "diaries/day_12.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_12.jpg"
+  },
+  {
+    "day": 13,
+    "start": "Burgos",
+    "end": "Hontanas",
+    "distance": 35.0,
+    "diaryFile": "diaries/day_13.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_13.jpg"
+  },
+  {
+    "day": 14,
+    "start": "Hontanas",
+    "end": "Boadilla del Camino",
+    "distance": 30.9,
+    "diaryFile": "diaries/day_14.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_14.jpg"
+  },
+  {
+    "day": 15,
+    "start": "Boadilla del Camino",
+    "end": "Carri\u00f3n de los Condes",
+    "distance": 28.5,
+    "diaryFile": "diaries/day_15.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_15.jpg"
+  },
+  {
+    "day": 16,
+    "start": "Carri\u00f3n de los Condes",
+    "end": "Moratinos",
+    "distance": 32.1,
+    "diaryFile": "diaries/day_16.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_16.jpg"
+  },
+  {
+    "day": 17,
+    "start": "Moratinos",
+    "end": "El Burgo Ranero",
+    "distance": 30.9,
+    "diaryFile": "diaries/day_17.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_17.jpg"
+  },
+  {
+    "day": 18,
+    "start": "El Burgo Ranero",
+    "end": "L\u00e9on",
+    "distance": 39.1,
+    "diaryFile": "diaries/day_18.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_18.jpg"
+  },
+  {
+    "day": 19,
+    "start": "L\u00e9on",
+    "end": "Villar de Mazarife",
+    "distance": 28.9,
+    "diaryFile": "diaries/day_19.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_19.jpg"
+  },
+  {
+    "day": 20,
+    "start": "Villar de Mazarife",
+    "end": "Astorga",
+    "distance": 34.4,
+    "diaryFile": "diaries/day_20.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_20.jpg"
+  },
+  {
+    "day": 21,
+    "start": "Astorga",
+    "end": "Foncebad\u00f3n",
+    "distance": 28.3,
+    "diaryFile": "diaries/day_21.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_21.jpg"
+  },
+  {
+    "day": 22,
+    "start": "Foncebad\u00f3n",
+    "end": "Ponferrada",
+    "distance": 30.0,
+    "diaryFile": "diaries/day_22.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_22.jpg"
+  },
+  {
+    "day": 23,
+    "start": "Ponferrada",
+    "end": "Villafranca del Bierzo",
+    "distance": 26.8,
+    "diaryFile": "diaries/day_23.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_23.jpg"
+  },
+  {
+    "day": 24,
+    "start": "Villafranca del Bierzo",
+    "end": "O Cebreiro",
+    "distance": 32.2,
+    "diaryFile": "diaries/day_24.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_24.jpg"
+  },
+  {
+    "day": 25,
+    "start": "O Cebreiro",
+    "end": "Triacastela",
+    "distance": 22.9,
+    "diaryFile": "diaries/day_25.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_25.jpg"
+  },
+  {
+    "day": 26,
+    "start": "Triacastela",
+    "end": "Sarria",
+    "distance": 19.0,
+    "diaryFile": "diaries/day_26.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_26.jpg"
+  },
+  {
+    "day": 27,
+    "start": "Sarria",
+    "end": "Portomarin",
+    "distance": 24.1,
+    "diaryFile": "diaries/day_27.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_27.jpg"
+  },
+  {
+    "day": 28,
+    "start": "Portomarin",
+    "end": "Palas de Rei",
+    "distance": 26.7,
+    "diaryFile": "diaries/day_28.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_28.jpg"
+  },
+  {
+    "day": 29,
+    "start": "Palas de Rei",
+    "end": "Arz\u00faa",
+    "distance": 34.0,
+    "diaryFile": "diaries/day_29.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_29.jpg"
+  },
+  {
+    "day": 30,
+    "start": "Arz\u00faa",
+    "end": "O Pedrouzo",
+    "distance": 21.6,
+    "diaryFile": "diaries/day_30.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_30.jpg"
+  },
+  {
+    "day": 31,
+    "start": "O Pedrouzo",
+    "end": "SANTIAGO DE COMPOSTELA ",
+    "distance": 21.3,
+    "diaryFile": "diaries/day_31.md",
+    "photo": "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_31.jpg"
+  }
+];
--- process_gpx.py
+++ process_gpx.py
@@ -4,22 +4,18 @@ import os
 import gpxpy
 import gpxpy.gpx
 import re
+import json
 
 # --- Configuration ---
 GPX_DIRECTORY = 'gpx_files' 
-OUTPUT_FILE = 'camino-data.js'
+OUTPUT_DATA_DIR = 'data'
+OUTPUT_METADATA_FILE = 'camino-metadata.js'
 SIMPLIFICATION_FACTOR = 10
 
 def parse_gpx_files(directory):
-    """
-    Parses all GPX files, extracts metadata from the filename,
-    calculates the distance, and extracts full and simplified paths.
-    """
     all_tracks = []
     gpx_files = [f for f in os.listdir(directory) if f.endswith('.gpx')]
     
-    # This regex is designed to capture the day number, start, and end locations
-    # from your specific filename format.
     filename_pattern = re.compile(r'Day_(\d+)_(.*)_to_(.*)\.gpx', re.IGNORECASE)
     
     def get_day_number(filename):
@@ -38,7 +34,6 @@ def parse_gpx_files(directory):
             continue
 
         day_index = int(match.group(1))
-        # Replace underscores with spaces for clean place names
         start_location = match.group(2).replace('_', ' ')
         end_location = match.group(3).replace('_', ' ')
         
@@ -47,15 +42,13 @@ def parse_gpx_files(directory):
         with open(filepath, 'r') as gpx_file:
             gpx = gpxpy.parse(gpx_file)
             
-            # --- NEW: Calculate distance ---
-            distance_meters = gpx.length_3d() # Uses 3D length (includes elevation)
-            distance_km = round(distance_meters / 1000, 1)
+            distance_km = round(gpx.length_3d() / 1000, 1)
 
             points_full = []
             for track in gpx.tracks:
                 for segment in track.segments:
                     for point in segment.points:
-                        points_full.append([point.longitude, point.latitude])
+                        points_full.append([round(point.longitude, 6), round(point.latitude, 6)])
             
             points_simple = points_full[::SIMPLIFICATION_FACTOR]
             if points_full and points_full[-1] != points_simple[-1]:
@@ -73,52 +66,62 @@ def parse_gpx_files(directory):
 
     return all_tracks
 
-def write_js_file(tracks, output_file):
+def write_data_files(tracks):
     """
-    Writes the fully populated track data into a JavaScript file.
+    Writes the geographic data to individual JSON files and the
+    metadata to a single JavaScript file.
     """
-    js_content = "const caminoData = [\n"
-    
-    for track in tracks:
-        js_content += "  {\n"
-        js_content += f"    day: {track['day']},\n"
-        js_content += f"    start: '{track['start']}',\n"
-        js_content += f"    end: '{track['end']}',\n"
-        js_content += f"    distance: {track['distance']},\n"
-        js_content += f"    description: '', // This will be loaded from the diary file\n"
-        js_content += f"    diaryFile: 'diaries/day_{track['day']:02d}.md',\n" # e.g., diaries/day_01.md
-        js_content += f"    photo: 'https://placehold.co/600x400/cccccc/ffffff?text=Day+{track['day']}',\n"
-        
-        js_content += "    path_full: [\n"
-        # To keep the file size reasonable, we'll only write a few points as a sample here.
-        # The script will write ALL points when you run it.
-        for i, point in enumerate(track['points_full']):
-            lon = round(point[0], 6)
-            lat = round(point[1], 6)
-            js_content += f"      [{lon}, {lat}]"
-            js_content += ",\n" if i < len(track['points_full']) - 1 else "\n"
-        js_content += "    ],\n"
+    if not os.path.exists(OUTPUT_DATA_DIR):
+        os.makedirs(OUTPUT_DATA_DIR)
+        print(f"Created directory: '{OUTPUT_DATA_DIR}/'")
 
-        js_content += "    path_simple: [\n"
-        for i, point in enumerate(track['points_simple']):
-            lon = round(point[0], 6)
-            lat = round(point[1], 6)
-            js_content += f"      [{lon}, {lat}]"
-            js_content += ",\n" if i < len(track['points_simple']) - 1 else "\n"
-        js_content += "    ]\n"
-        
-        js_content += "  },\n"
-        
-    js_content += "];\n"
-    
-    with open(output_file, 'w') as f:
+    metadata = []
+
+    # Add Day 0 metadata manually
+    metadata.append({
+        'day': 0,
+        'start': "Arrival in",
+        'end': "Saint-Jean-Pied-de-Port",
+        'distance': 0,
+        'diaryFile': "diaries/day_00.md",
+        'photo': "https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_00.jpg",
+        'center_coord': [43.1636, -1.2372],
+        'zoom_level': 15
+    })
+
+    for track in tracks:
+        # Create metadata entry (without path data)
+        metadata.append({
+            'day': track['day'],
+            'start': track['start'],
+            'end': track['end'],
+            'distance': track['distance'],
+            'diaryFile': f"diaries/day_{track['day']:02d}.md",
+            'photo': f"https://raw.githubusercontent.com/sophiefsadler/Camino_Map/main/images/day_{track['day']:02d}.jpg",
+        })
+
+        # Create the JSON file for the path data
+        path_data = {
+            'path_full': track['points_full'],
+            'path_simple': track['points_simple']
+        }
+        json_filename = f"day_{track['day']:02d}.json"
+        json_filepath = os.path.join(OUTPUT_DATA_DIR, json_filename)
+        with open(json_filepath, 'w') as f:
+            json.dump(path_data, f)
+
+    # Write the metadata file
+    js_content = "const caminoMetadata = " + json.dumps(metadata, indent=2) + ";\n"
+    with open(OUTPUT_METADATA_FILE, 'w') as f:
         f.write(js_content)
-    print(f"\nSuccessfully created JavaScript data file: {output_file}")
+    
+    print(f"\nSuccessfully created metadata file: {OUTPUT_METADATA_FILE}")
+    print(f"Successfully created {len(tracks)} JSON data files in '{OUTPUT_DATA_DIR}/'.")
 
 
 if __name__ == '__main__':
     tracks_data = parse_gpx_files(GPX_DIRECTORY)
     if tracks_data:
-        write_js_file(tracks_data, OUTPUT_FILE)
+        write_data_files(tracks_data)
     else:
         print("No GPX files found. Please check the 'gpx_files' directory.")
