2ae20149c1c93c724ad479d80811905b51991be5
Sophie Sadler <sophiefsadler@gmail.com>
2025-07-30 16:49:52 +0200

    Added first test marker

---
+++ poi-data.js
@@ -0,0 +1,9 @@
+const poiData = [
+  {
+    day: 1,
+    title: "The Start of the Camino",
+    description: "About to set off on my big adventure!",
+    photoUrl: "https://github.com/sophiefsadler/Camino_Map/blob/main/images/story_panel/Day_00.jpg?raw=true",
+    coordinates: [43.162417, -1.237222]
+  }
+];
