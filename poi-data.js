2b5606bf1b42c359d2773bd342838895c9c05597
Sophie Sadler <sophiefsadler@gmail.com>
2025-08-02 22:29:07 +0100

    Added POIs for day 1

--- poi-data.js
+++ poi-data.js
@@ -3,7 +3,42 @@ const poiData = [
     day: 1,
     title: "The Start of the Camino",
     description: "About to set off on my big adventure!",
-    photoUrl: "https://github.com/sophiefsadler/Camino_Map/blob/main/images/story_panel/Day_00.jpg?raw=true",
+    photoUrl: "https://github.com/sophiefsadler/Camino_Map/blob/main/images/poi/day_1/camino_start.jpg?raw=true",
     coordinates: [43.162417, -1.237222]
-  }
+  },
+  {
+    day: 1,
+    title: "Horses in the Pyrenees",
+    description: "The Camino starts with amazing views and lots of cute animals",
+    photoUrl: "https://github.com/sophiefsadler/Camino_Map/blob/main/images/poi/day_1/horses_pyrenees.jpg?raw=true",
+    coordinates: [43.151669, -1.255244]
+  },
+  {
+    day: 1,
+    title: "The village of Pekotxeta",
+    description: "On the right side of the river is Spain; on the left side of the river is France!",
+    photoUrl: "https://github.com/sophiefsadler/Camino_Map/blob/main/images/poi/day_1/france_spain_river.jpg?raw=true",
+    coordinates: [43.110742, -1.280233]
+  },
+  {
+    day: 1,
+    title: "A view of the hills from Valcarlos",
+    description: "Valcarlos is roughly halfway through the day's walking. Some pilgrims even stay here and split the day in two.",
+    photoUrl: "https://github.com/sophiefsadler/Camino_Map/blob/main/images/poi/day_1/valcarlos.jpg?raw=true",
+    coordinates: [43.092336, -1.302158]
+  },
+  {
+    day: 1,
+    title: "The River Luzaide",
+    description: "This was probably my favourite part of the day.",
+    photoUrl: "https://github.com/sophiefsadler/Camino_Map/blob/main/images/poi/day_1/walking_by_river.jpg?raw=true",
+    coordinates: [43.063644, -1.306406]
+  },
+  {
+    day: 1,
+    title: "The Monumento a Roldán",
+    description: "We made it to the top of the big hill!",
+    photoUrl: "https://github.com/sophiefsadler/Camino_Map/blob/main/images/poi/day_1/monument.jpg?raw=true",
+    coordinates: [43.020589, -1.323442]
+  },
 ];
